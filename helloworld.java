c098c83514cb00c8f30394c6eba738c1aefa5535
Mark Milligan <mtm20176@gmail.com>
2021-03-19 14:43:22 +0000

    more println

--- helloworld.java
+++ helloworld.java
@@ -7,14 +7,18 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World!");
-        System.out.println("You watching a Coder development environment");
+        System.out.println("");
+        System.out.println("You are watching a Coder development environment");
         System.out.println("running in a dedicated container and pod");
         System.out.println("in a cloud-based Kubernetes cluster");
         System.out.println("");
-        System.out.println("You probably using a browser-based Terminal session");
+        System.out.println("You are probably using a browser-based Terminal session");
         System.out.println("or SSHed in from your local machine");
+        System.out.println("");
         System.out.println("and were changing the Java file with");
-        System.out.println("a JetBrains editor in a browser");
+        System.out.println("a JetBrains IntelliJ editor in a browser");
         System.out.println("or maybe even local Eclipse through SSH");
+        System.out.println("");
+        System.out.println("");
     } 
 } 
