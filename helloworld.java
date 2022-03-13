9c22e71489a4feb7ac64444fb3672d5cfc4c0c49
Mark Milligan <mtm20176@gmail.com>
2022-03-13 12:40:58 +0000

    adjust args; add a package; to resolve intellij warnings

--- helloworld.java
+++ helloworld.java
@@ -1,11 +1,12 @@
+package java_helloworld;
 
 /* This is a simple Hello World! Java program */
 class HelloWorld 
 {
     // Your program begins with a call to main(). 
     // Prints "Hello, World!" to the terminal window. 
-    public static void main(String args[]) 
+    public static void main(String[] args)
     { 
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World JetBrains IntelliJ!");
     } 
 } 
